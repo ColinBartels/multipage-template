491cb346a1f86f638d78b49e09429fbff8308aa1
Colin Bartels <colin.0011@hotmail.com>
2015-11-10 09:46:29 -0800

    first commit

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
 // Create app
-
+var myApp = angular.module('myApp', ['ui.router']);
 // Configure app
+myApp.config(function($stateProvider) {
+    $stateProvider
+	.state('home', {
+		url:'/',
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController',
+	})
+	.state('about', {
+		url:'/about',
+		templateUrl: 'templates/about.html',
+		controller: 'AboutController'
+	})
+	.state('content', {
+		url: '/content',
+		templateUrl: 'templates/content.html',
+		controller: 'ContentController'
+	});
+});
+	// Landing page controller: define $scope.number as a number
+	var HomeController = myApp.controller('HomeController', function($scope) {
+		$scope.number = "2";
+	});
 
-// Landing page controller: define $scope.number as a number
+	// About page controller: define $scope.about as a string
+	var aboutController = myApp.controller('AboutController', function($scope) {
+		$scope.about = "this is a string";
+	});
 
-
-// About page controller: define $scope.about as a string
-
-
-// Content controller: define $scope.url as an image
-
-
+	// Content controller: define $scope.url as an image
+	var contentController = myApp.controller('ContentController', function($scope) {
+		$scope.url = "http://cdn-img.people.com/emstag/i/2012/pets/news/120430/penguin-1-440.jpg";
+	});
